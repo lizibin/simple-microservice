2088d23315e387ea5e5db99de940707b9dd9b1c0
lizibin <125720240@qq.com>
2019-06-04 00:21:40 +0800

    新增基础架构和服务

---
+++ eureka-service/src/main/java/com/ctnrs/eureka/EurekaService.java
@@ -0,0 +1,16 @@
+/**
+ * <p>Company: http://www.ctnrs.com/</p>
+ * <p>Copyright: Copyright (c) 2019</p>
+ */
+package com.ctnrs.eureka;
+
+/**
+ * eureka服务启动类
+ * @author zibin
+ */
+public class EurekaService {
+
+    public static void main(String[] args) {
+
+    }
+}
